284fae71f5b34b246d51939ff496d06a6289c233
RGunny <sadog94@gmail.com>
2022-01-04 23:06:34 +0900

    [refactor/baseball] : delete duplicated test code

--- number-baseball/src/test/java/baseball/domain/RefereeTest.java
+++ number-baseball/src/test/java/baseball/domain/RefereeTest.java
@@ -1,7 +1,8 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,40 +18,48 @@ class RefereeTest { // Class 이름 + Test (camel)
         referee = new Referee();
     }
 
-    @Test
-    void 스트라이크3() {
-//        Referee referee = new Referee();
-//        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
-        String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
-        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
-        assertThat(result).isEqualTo("0 볼 3 스트라이크"); // assert : 주장하다
-//        assertThat(result).isEqualTo("0 볼 2 스트라이크");
+    // "0 볼 3 스트라이크" => 얘도 중복이다 => JUnit5 부터 @ParameterizedTest 여러가지 경우에 대해 테스트 할 수 있음
+    @ParameterizedTest
+    @CsvSource({"1,2,3,0 볼 3 스트라이크", "7,8,9,아웃", "2,3,1,3 볼 0 스트라이크", "3,2,1,2 볼 1 스트라이크"})
+    void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assertThat(actual).isEqualTo(expected); // assert : 주장하다
     }
-
-    @Test
-    void 아웃() {
-//        Referee referee = new Referee();
-//        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7,8,9));
-        String result = referee.compare(ANSWER, Arrays.asList(7,8,9));
-        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
-        assertThat(result).isEqualTo("아웃"); // assert : 주장하다
-    }
-
-    @Test
-    void 볼3() {
-//        Referee referee = new Referee();
-//        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
-        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
-        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
-        assertThat(result).isEqualTo("3 볼 0 스트라이크"); // assert : 주장하다
-    }
-
-    @Test
-    void 볼2_스트라이크1() {
-//        Referee referee = new Referee();
-//        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 2, 1));
-        String result = referee.compare(ANSWER, Arrays.asList(3, 2, 1));
-        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
-        assertThat(result).isEqualTo("2 볼 1 스트라이크"); // assert : 주장하다
-    }
+//
+//    @Test
+//    void 스트라이크3() {
+////        Referee referee = new Referee();
+////        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+//        String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
+//        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
+//        assertThat(result).isEqualTo("0 볼 3 스트라이크"); // assert : 주장하다
+////        assertThat(result).isEqualTo("0 볼 2 스트라이크");
+//    }
+//
+//    @Test
+//    void 아웃() {
+////        Referee referee = new Referee();
+////        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(7,8,9));
+//        String result = referee.compare(ANSWER, Arrays.asList(7,8,9));
+//        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
+//        assertThat(result).isEqualTo("아웃"); // assert : 주장하다
+//    }
+//
+//    @Test
+//    void 볼3() {
+////        Referee referee = new Referee();
+////        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 1));
+//        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
+//        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
+//        assertThat(result).isEqualTo("3 볼 0 스트라이크"); // assert : 주장하다
+//    }
+//
+//    @Test
+//    void 볼2_스트라이크1() {
+////        Referee referee = new Referee();
+////        String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(3, 2, 1));
+//        String result = referee.compare(ANSWER, Arrays.asList(3, 2, 1));
+//        System.out.println("result = " + result); // 이렇게 사람이 확인하는 것이 아닌,
+//        assertThat(result).isEqualTo("2 볼 1 스트라이크"); // assert : 주장하다
+//    }
 }
