1f44e1ebae494cdcc7d17a3b0d6f599fd78a2e44
RGunny <sadog94@gmail.com>
2022-02-21 22:55:11 +0900

    feat(java) : find lambda quiz

--- java-test/src/test/java/me/rgunny/study/lambda/LambdaTestTest.java
+++ java-test/src/test/java/me/rgunny/study/lambda/LambdaTestTest.java
@@ -4,11 +4,25 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LambdaTestTest {
+
+    @Test
+    void findLambdaQuiz(){
+        Runnable runnable = () -> {}; // 파라미터가 없으며 void를 반환하는 바디가 없는 람다 표현식
+        Callable<String> stringCallable = () -> "Gunny"; // 파라미터가 없으며 문자열을 반환하는 표현식
+        Callable<String> stringCallable1 = () -> {return "Gunny";}; // 파라미터가 없으며 명시적으로 return 문을 이용해 문자열을 반환하는 표현식
+//        (Integer i) -> return "Gunny" + i; // return은 흐름 제어문이다.
+        Function<Integer, String> function = (Integer i) -> {return "Gunny" + i;};
+//        (String s) -> {"Gunny";} // "Gunny"는 구문(statement)이 아니라 표현식(expression)이다.
+        Function<String, String> function1 = (String s) -> "Gunny"; // 올바른 람다 표현식
+        Function<String, String> function2 = (String s) -> {return "Gunny";}; // 또는 명시적으로 return 문 사용
+    }
 
     @Test
     void lambda3Expressions() {
