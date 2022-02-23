902e59977b118f31ff5e514cd6fbc909d5643939
RGunny <sadog94@gmail.com>
2022-02-23 19:48:21 +0900

    feat(java) : method reference using constructor test

--- java-test/src/test/java/me/rgunny/study/lambda/MethodReferenceTest.java
+++ java-test/src/test/java/me/rgunny/study/lambda/MethodReferenceTest.java
@@ -2,6 +2,8 @@ package me.rgunny.study.lambda;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,5 +41,36 @@ class MethodReferenceTest {
 
         // 생성된 hello로 apply()해야 "gunny"라는 값이 Greeting 클래스의 hello 메서드에 전달되어 출력
         System.out.println(hello.apply("gunny"));
+    }
+
+    /**
+     * use method reference (빈 constructor 참조)
+     */
+    @Test
+    void defaultConstructor(){
+        // 생성자를 호출할 때 리턴은 객체의 타입 => 입력값은 없는데, 결과같은 있음 => Supplier
+        // 아직 인스턴스(Greeting) 아님 Supplier임
+        Supplier<Greeting> newGreeting = Greeting::new;
+        Greeting greeting = newGreeting.get(); // Supplier => Greeting 생성
+        System.out.println("greeting = " + greeting);
+        System.out.println("greeting.getName() = " + greeting.getName());
+        System.out.println("greeting.getClass() = " + greeting.getClass());
+        assertThat(greeting.getName()).isNull();
+    }
+
+    /**
+     * use method reference (이름을 받는 constructor 참조)
+     */
+    @Test
+    void constructor() {
+        // 입력(인자)과 리턴이 다름 => Function
+        Function<String, Greeting> gunnyGreeting = Greeting::new;
+
+        // 위 newGreeting과 gunnyGreeting은 서로 다른 생성자를 참조하고 있음
+        // => 메서드 레퍼런스로는 호출하는 생성자를 판단하기 힘들다.
+        Greeting gunny = gunnyGreeting.apply("gunny");
+        System.out.println(gunny.getName()); // 문자를 받는 생성자가 생성된 것을 확인
+        System.out.println("gunny = " + gunny);
+        assertThat(gunnyGreeting.apply("gunny").getName().equals("gunny")).isTrue();
     }
 }
