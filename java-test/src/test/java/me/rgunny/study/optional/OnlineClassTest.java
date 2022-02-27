38ebe3e9bc2601b46c8bb1f7facbed1a748027b4
RGunny <sadog94@gmail.com>
2022-02-27 20:41:35 +0900

    feat(java) : Optional init

---
+++ java-test/src/test/java/me/rgunny/study/optional/OnlineClassTest.java
@@ -0,0 +1,23 @@
+package me.rgunny.study.optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OnlineClassTest {
+
+    @BeforeEach
+    void setUp() {
+        List<Object> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+    }
+
+}
