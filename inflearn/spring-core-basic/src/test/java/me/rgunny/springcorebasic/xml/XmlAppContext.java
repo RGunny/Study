3321d0a5d6a1aa12930ca4ecc1321ebdf8daa5e6
RGunny <sadog94@gmail.com>
2022-07-19 21:31:51 +0900

    test(spring-core-basic) : 다양한 설정 혁식 지원 - 자바 코드, XML

---
+++ inflearn/spring-core-basic/src/test/java/me/rgunny/springcorebasic/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package me.rgunny.springcorebasic.xml;
+
+import me.rgunny.springcorebasic.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("XML을 활용한 빈 설정 (appConfig.xml)")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
