53c0441a580c54c7e87e42a9f4d6490322adcda9
RGunny <sadog94@gmail.com>
2022-07-19 22:51:21 +0900

    test(spring-core-basic) : 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ inflearn/spring-core-basic/src/test/java/me/rgunny/springcorebasic/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,40 @@
+package me.rgunny.springcorebasic.beandefinition;
+
+import me.rgunny.springcorebasic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext xac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인 (xml 설정)")
+    void findApplicationBeanByXml() {
+        String[] beanDefinitionNames = xac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = xac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
