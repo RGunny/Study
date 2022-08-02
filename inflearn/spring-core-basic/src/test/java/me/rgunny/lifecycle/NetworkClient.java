ca41e4439be8faa549223ce8badcb663b0e543b0
RGunny <sadog94@gmail.com>
2022-08-02 23:26:05 +0900

    test(spring-core-basic) : 애노테이션 @PostConstruct, @PreDestroy

--- inflearn/spring-core-basic/src/test/java/me/rgunny/lifecycle/NetworkClient.java
+++ inflearn/spring-core-basic/src/test/java/me/rgunny/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package me.rgunny.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * 초기화, 소명 인터페이스 단점 (InitializingBean, DisposableBean)
  * - 스프링 전용 인터페이스 -> 해당 코드가 스프링 전용 인터페이스에 의존
@@ -11,6 +14,13 @@ package me.rgunny.lifecycle;
  * - 메서드 이름을 자유롭게 변경 가능
  * - 스프링 빈이 스프링 코드에 의존하지 않음
  * - 코드가 아닌, 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.
+ *
+ * @PostConstruct, @PreDestroy 애노테이션 특징
+ * - 최신 스프링에서 가장 권장하는 방법
+ * - 애노테이션 하나만 붙이면 되므로 매우 편리
+ * - `javax.annotation.PostConstruct` -> javax.~ 패키지는 스프링에 종속적인 기술이 아닌, `JSR-250` 이라는 자바 표준이다. -> 스프링이 아닌 다른 컨테이너에서도 동작
+ * - 컴포넌트 스캔과 잘 어울림
+ * - 유일한 단점: 외부 라이브러리에는 적용하지 못함. 외부 라이브러리를 초기화, 종료 해야 하면 @Bean 의 기능을 사용해야 함(@Bean initMethod, destroyMethod)
  */
 //public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
@@ -43,6 +53,7 @@ public class NetworkClient {
     // 의존관계 주입 종료 후 호출
 //    @Override
 //    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
@@ -53,6 +64,7 @@ public class NetworkClient {
     // 빈이 료될 때 호출
 //    @Override
 //    public void destroy() throws Exception {
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
