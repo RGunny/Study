6be2f6d3413abf06d44ea8318162a026bae2c917
RGunny <sadog94@gmail.com>
2022-08-14 15:41:30 +0900

    feat(querydsl) : Querydsl 설정과 검증

--- inflearn/querydsl/src/test/java/me/rgunny/querydsl/QuerydslApplicationTests.java
+++ inflearn/querydsl/src/test/java/me/rgunny/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package me.rgunny.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.rgunny.querydsl.entity.Hello;
+import me.rgunny.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+//    @Autowired
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
