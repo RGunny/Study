28d2c5a032c35a39787cab4374452cfb372f0ef2
RGunny <sadog94@gmail.com>
2022-08-16 22:43:42 +0900

    feat(querydsl) : 예제 도메인 모델과 동작확인

---
+++ inflearn/querydsl/src/main/java/me/rgunny/querydsl/entity/Team.java
@@ -0,0 +1,29 @@
+package me.rgunny.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 PROTECTED level 생성
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
