415839e723142d559bd245ed33cfae31df095aee
RGunny <sadog94@gmail.com>
2022-08-16 22:20:50 +0900

    feat(querydsl) : 스프링 부트 설정 - JPA, DB(H2)

--- inflearn/querydsl/src/test/java/me/rgunny/querydsl/QuerydslApplicationTests.java
+++ inflearn/querydsl/src/test/java/me/rgunny/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import me.rgunny.querydsl.entity.Hello;
 import me.rgunny.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 //    @Autowired
