9ddaa1ddc7ce1980e03f87c7774a0ab485ff2208
RGunny <sadog94@gmail.com>
2022-08-18 00:17:13 +0900

    test(querydsl) : JPQL vs QUerydsl

---
+++ inflearn/querydsl/src/test/java/me/rgunny/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,80 @@
+package me.rgunny.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.rgunny.querydsl.entity.Member;
+import me.rgunny.querydsl.entity.QMember;
+import me.rgunny.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    /**
+     * JPAQueryFactory 필드 제공시 동시성 문제
+     * - 동시성 문제는 JPAQueryFactory 를 생성할 때 제공하는 EntityManager(em)에 달려있다.
+     * - 스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager 에 접근해도, 트랜잭션 마다 별도의 영속성 컨텍스트를 제공하기 때문에, 동시성 문제는 걱정하지 않아도 된다.
+     */
+    JPAQueryFactory queryFactory;
+
+
+    @BeforeEach
+    void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    void startJPQL() {
+        // find member1
+        String qlString =
+                "select m from Member m where m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void startQuerydsl() {
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .selectFrom(m)
+                .from(m)
+                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+
+}
