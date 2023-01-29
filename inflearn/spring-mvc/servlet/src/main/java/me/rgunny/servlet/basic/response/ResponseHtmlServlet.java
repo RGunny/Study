dc753b067ff5954090b8d9db64ac60790fb22772
RGunny <sadog94@gmail.com>
2023-01-29 20:50:47 +0900

    feat(servlet) : Http 응답 데이터 - 단순 텍스트, html

--- inflearn/spring-mvc/servlet/src/main/java/me/rgunny/servlet/basic/response/ResponseHtmlServlet.java
+++ inflearn/spring-mvc/servlet/src/main/java/me/rgunny/servlet/basic/response/ResponseHtmlServlet.java
@@ -14,7 +14,7 @@ public class ResponseHtmlServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Content-Type: text/html;charset=utf-9
-        response.setContentType("text/html");
+        response.setContentType("text/html"); // HTTP 응답으로 HTML을 반환할 때는 content-type을 text/html 로 지정해야 함
         response.setCharacterEncoding("utf-8");
 
         PrintWriter writer = response.getWriter();
