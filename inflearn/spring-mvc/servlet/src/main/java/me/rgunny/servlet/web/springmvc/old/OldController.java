fe89028a7e2d97267135e08fa0e705e49d1841e5
RGunny <sadog94@gmail.com>
2023-03-04 14:46:59 +0900

    feat(servlet) - ViewResolver

--- inflearn/spring-mvc/servlet/src/main/java/me/rgunny/servlet/web/springmvc/old/OldController.java
+++ inflearn/spring-mvc/servlet/src/main/java/me/rgunny/servlet/web/springmvc/old/OldController.java
@@ -83,6 +83,6 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return new ModelAndView("new-form"); // 논리적 이름 new-form 을 넣어 viewResolver 에서 물리적 이름 /WEB-INF/views/new-form 반환ㄲ
+        return new ModelAndView("new-form"); // 논리적 이름 new-form 을 넣어 viewResolver 에서 물리적 이름 /WEB-INF/views/new-form 반환
     }
 }
