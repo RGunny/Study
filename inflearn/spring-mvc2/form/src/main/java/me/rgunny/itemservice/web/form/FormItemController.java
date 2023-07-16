39811421683c84394d3b4ba3ab423950f54fb2c1
RGunny <sadog94@gmail.com>
2023-07-16 23:46:02 +0900

    feat(mvc2-form) : 체크 박스 - 단일1

--- inflearn/spring-mvc2/form/src/main/java/me/rgunny/itemservice/web/form/FormItemController.java
+++ inflearn/spring-mvc2/form/src/main/java/me/rgunny/itemservice/web/form/FormItemController.java
@@ -1,6 +1,7 @@
 package me.rgunny.itemservice.web.form;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.rgunny.itemservice.domain.item.Item;
 import me.rgunny.itemservice.domain.item.ItemRepository;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +41,11 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        // 체크박스 선택시 HTML Form 에서 'open=on' 값이 넘어가는데, 스프링이 'on' 문자를 'true' 타입으로 변환해줌 (스프링 타입 컨버터 기능)
+        // 체크박스 미선택시, 아예 값이 안넘어감 --> 'open=null'
+        // open=on&_open=on 입력시 스프링 MVC가 open 에 겂이 있는 것을 확인하고 _open 무시
+        // _open=on 만 입력시 스프링 MVC가 _open 만 있는 것을 확인하고 open 값이 체크되지 않았음을 인식함 --> 서버에서 Boolean 확인시 null 이 아닌 false
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
