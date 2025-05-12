d094f1dfc58a01ac9ed3638589ae719b8b6d246a
RGunny <sadog94@gmail.com>
2025-05-12 21:38:23 +0900

    feat(java-adv3) : 람다가 필요한 이유

---
+++ inflearn/java-adv3/src/lambda/start/Ex0RefMain.java
@@ -0,0 +1,15 @@
+package lambda.start;
+
+public class Ex0RefMain {
+
+    public static void main(String[] args) {
+        hello("Hello Java");
+        hello("Hello Spring");
+    }
+
+    public static void hello(String msg){
+        System.out.println("프로그램 시작");
+        System.out.println("Hello Spring");
+        System.out.println("프로그램 종료");
+    }
+}
