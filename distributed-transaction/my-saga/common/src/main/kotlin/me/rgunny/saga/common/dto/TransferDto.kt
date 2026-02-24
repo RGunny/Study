f981db1d4be6782b3fb34ec5457d8c67d6943679
RGunny <sadog94@gmail.com>
2026-02-24 11:44:14 +0900

    feat(saga): add common DTOs and Kafka event classes
    Shared DTOs (Transfer, Deposit, Notification) and
    Kafka events (Withdraw/Deposit Success/Failed).

---
+++ distributed-transaction/my-saga/common/src/main/kotlin/me/rgunny/saga/common/dto/TransferDto.kt
@@ -0,0 +1,16 @@
+package me.rgunny.saga.common.dto
+
+import java.math.BigDecimal
+
+data class TransferRequest(
+    val fromAccountNumber: String,
+    val toAccountNumber: String,
+    val amount: BigDecimal
+)
+
+data class TransferResponse(
+    val sagaId: String,
+    val status: String,
+    val message: String
+
+)
